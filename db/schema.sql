3b6db8380a130e4febc58b496b06d3a46d08b5ec
X <jf13@x.lan>
2017-08-21 16:17:28 -0500

    db

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+USE burgers_db;
+
+
+CREATE TABLE burgers (
+	id INTEGER(11) NOT NULL AUTO_INCREMENT,
+	burger_name VARCHAR(30),
+	devoured BOOLEAN DEFAULT false,
+	date TIMESTAMP NOT NULL,
+	PRIMARY KEY(id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name)
+
+VALUES
+("all american bacon burger"),
+("avocado ranch bacon burger"),
+("random surprise burger");
